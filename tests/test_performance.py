6dc1721f61dba074714bf6e95d0a0ede630a12df
Justin Payne <crashfrog@gmail.com>
2020-05-06 11:51:48 -0400

    performance testing

---
+++ tests/test_performance.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+
+
+
+
+class TestPerformance(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testPerformanceOnNFiles(self, n=3000):
+        pass
